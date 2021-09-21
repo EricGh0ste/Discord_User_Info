b00dc5646cbdb204d21653c8b046d25ea908269e
void* (GostiA) <42337469+EricGh0ste@users.noreply.github.com>
2021-09-22 02:04:29 +0300

    Add files via upload

---
+++ Discord_User_Info/Discord_User_Info/Classes/Discord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Discord_User_Info.Classes
+{
+    class Discord
+    {
+        public static string get_info_by_id(string id)
+        {
+            JObject obj;
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Set("Authorization", "Bot ODg2NTg2NDE3MzAyNjE0MDY3.YT3v8Q.uFC1k-r91-w_npu6XHjObUxXYE4");
+                try
+                {
+                    string res = wc.DownloadString($"https://discord.com/api/v8/users/{id}");
+                    obj = JObject.Parse(res);
+                    obj.Add(new JProperty("status", "ok"));
+                    return obj.ToString();
+                }
+                catch (Exception)
+                {
+                    obj = JObject.Parse("{\"status\": \"notok\"}");
+                    return obj.ToString();
+                }
+            }
+        }
+
+        public static string get_info_by_token(string token)
+        {
+            JObject obj;
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Set("Authorization", token);
+                wc.Headers.Set("Content-Type", "application/json");
+                try
+                {
+                    byte[] res = wc.DownloadData($"https://discord.com/api/v6/users/@me");
+                    string response = Encoding.ASCII.GetString(res);
+                    obj = JObject.Parse(response);
+                    obj.Add(new JProperty("token", token));
+                    obj.Add(new JProperty("status", "ok"));
+                    return obj.ToString();
+                }
+                catch (Exception)
+                {
+                    obj = JObject.Parse("{\"status\": \"notok\"}");
+                    return obj.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ Discord_User_Info/Discord_User_Info/Classes/GConsole.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Discord_User_Info.Classes
+{
+    class GConsole
+    {
+        public static void clear()
+        {
+            Console.Clear();
+        }
+
+        public static void get_key()
+        {
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void print(string content)
+        {
+            Console.Write(content.ToString());
+        }
+
+        public static void print_ok(string content)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"] {content.ToString()}");
+        }
+
+        public static void print_err(string content)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("-");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"] {content.ToString()}");
+        }
+
+        public static void print_success(string content)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("+");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"] {content.ToString()}");
+        }
+
+        public static void print_opt(string prefix, string content)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(prefix);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"] {content.ToString()}");
+        }
+
+        public static string get_input()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("--> ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string inp = Console.ReadLine();
+            return inp.ToString();
+        }
+    }
+}
